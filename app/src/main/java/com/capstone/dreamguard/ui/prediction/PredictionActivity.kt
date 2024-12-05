05bfb57751f26f29dbd34bc95f8fb11adeebfdca
frhnrmdhny <farhanramadhany16@gmail.com>
2024-12-05 19:56:30 +0700

    Add All new Update UI

--- app/src/main/java/com/capstone/dreamguard/ui/prediction/PredictionActivity.kt
+++ app/src/main/java/com/capstone/dreamguard/ui/prediction/PredictionActivity.kt
@@ -1,16 +1,17 @@
-package com.capstone.dreamguard
+package com.capstone.dreamguard.ui.prediction
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.capstone.dreamguard.R
 
-class MainActivity : AppCompatActivity() {
+class PredictionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_prediction)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
